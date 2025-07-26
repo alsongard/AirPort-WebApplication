9f8a9083e5ecc0612596871ddf6554a913551edc
alsongard <alsongard@gmail.com>
2025-07-26 21:32:25 +0300

    user and userdetails added

---
+++ backend/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+
+const UserSchema = new mongoose.Schema(
+    {
+        email: {type:String, require:true, trim:true},
+        password: {type:String, require:true, trim:true},
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
---
+++ backend/models/userdetails.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const UserDetailSchema = new mongoose.Schema(
+    {
+        firstName: {type:String, required:true, trim:true},
+        middleName: {type:String, required:true, trim:true},
+        lastName: {type:String, required:true, trim:true},
+        userId: {ref:"User", type:mongoose.Schema.Types.ObjectId}
+    },
+    {
+        timestamps:true
+    }
+)
+
+const UserDetail = mongoose.model("UserDetail", UserDetailSchema);
+
+module.exports = UserDetail;
