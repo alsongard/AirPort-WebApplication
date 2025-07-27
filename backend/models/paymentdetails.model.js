f75b29ad6bb2c093c52acb3f06b0ab6dfc484db5
alsongard <alsongard@gmail.com>
2025-07-27 05:47:43 +0300

    payment model modified:add new fields

--- backend/models/paymentdetails.model.js
+++ backend/models/paymentdetails.model.js
@@ -5,7 +5,8 @@ const PayMentSchema = new mongoose.Schema(
     {
         bookingID: {type:mongoose.Schema.Types.ObjectId, ref:"Booking"},
         amountPaid: {type:Number, required:true},
-        paymentMthd: {type:String, required:true}
+        paymentMethod: {type:String, required:true},
+        paymentStatus:{type:String, required:true}
     },
     {
         timestamps: true
