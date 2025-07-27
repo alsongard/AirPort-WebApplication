74bc0eda081a8920adbb4afabd2d0e43c49b8cbe
alsongard <alsongard@gmail.com>
2025-07-27 03:38:50 +0300

    flight model added

---
+++ backend/models/flightdetails.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+
+const FlightSchema = new mongoose.Schema(
+    {
+        flightName:{type:String, required:true},
+        departure:{type:String, required:true},
+        destination:{type:String, required:true},
+        departureTime:{type:Date, required:true},
+        arrivalTime:{type:Date, required:true},
+        totalSeats:{type:Date, required:true},
+        price:{type:Number, required:true}
+    },
+    {
+        timestamps:true
+    }
+)
+
+const Flight = mongoose.model("Flight", FlightSchema);
+module.exports = Flight;
