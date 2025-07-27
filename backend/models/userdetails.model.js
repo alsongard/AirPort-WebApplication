500e4ba08db7685011843bd9c747c8665e2b5294
alsongard <alsongard@gmail.com>
2025-07-27 03:39:30 +0300

    userdetails added

--- backend/models/userdetails.model.js
+++ backend/models/userdetails.model.js
@@ -5,6 +5,8 @@ const UserDetailSchema = new mongoose.Schema(
         firstName: {type:String, required:true, trim:true},
         middleName: {type:String, required:true, trim:true},
         lastName: {type:String, required:true, trim:true},
+        age: {type:Number, required:true, trim:true},
+        passportNumber: {type:String, required:true, trim:true},
         userId: {ref:"User", type:mongoose.Schema.Types.ObjectId}
     },
     {
