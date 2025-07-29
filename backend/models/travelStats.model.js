4f1b95222c8e755c4eb05efee41624215b4dac95
alsongard <alsongard@gmail.com>
2025-07-29 03:23:13 +0300

    UserTravellingStats added

---
+++ backend/models/travelStats.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const UserTravellingStatsSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    totalTrips: {
+        type: Number,
+        default: 0
+    },
+    totalDistance: {
+        type: Number,
+        default: 0 // in kilometers
+    },
+    totalDuration: {
+        type: Number,
+        default: 0 // in hours
+    },
+    lastTripDate: {
+        type: Date
+    },
+    countriesVisited: {
+        type: Number,
+        default: 0
+    },
+    
+}, {
+    timestamps: true
+});
+
+const UserTravellingStats = mongoose.model('TravellingStat', UserTravellingStatsSchema);
+module.exports = UserTravellingStats;
