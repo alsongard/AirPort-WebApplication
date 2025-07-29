84dda6c8d64ea85584697a5b95e04c52d545f0ab
alsongard <alsongard@gmail.com>
2025-07-29 03:28:43 +0300

    added fields phone,dateBirth and emergency contact

--- backend/models/userdetails.model.js
+++ backend/models/userdetails.model.js
@@ -7,7 +7,10 @@ const UserDetailSchema = new mongoose.Schema(
         lastName: {type:String, required:true, trim:true},
         age: {type:Number, required:true, trim:true},
         passportNumber: {type:String, required:true, trim:true},
-        userId: {ref:"User", type:mongoose.Schema.Types.ObjectId}
+        userId: {ref:"User", type:mongoose.Schema.Types.ObjectId},
+        phone: {type:Number, required:true},
+        dateBirth: {type:Date, required:true},
+        emergencyContact:{type:Number, required:true}
     },
     {
         timestamps:true
@@ -15,5 +18,4 @@ const UserDetailSchema = new mongoose.Schema(
 )
 
 const UserDetail = mongoose.model("UserDetail", UserDetailSchema);
-
 module.exports = UserDetail;
