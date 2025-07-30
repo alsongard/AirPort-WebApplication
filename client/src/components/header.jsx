86524d14dca19ef73c9f26321aaff0d50cec9624
alsongard <alsongard@gmail.com>
2025-07-31 00:05:59 +0300

    header modified: use state:isLoggedIn for displaying navbar links

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,21 +1,23 @@
 import { FaMoon } from "react-icons/fa";
-import {NavLink} from 'react-router-dom';
-
-
-function Header(){
+import {NavLink, useNavigate} from 'react-router-dom';
+import { connect } from "react-redux";
+function Header(props){
+    const navigate = useNavigate();
+    const handleLogoutOut = ()=>{
+        props.onLoggingOut();
+        localStorage.clear();
+        navigate("/");
+    }
     return (
-        <header className="flex items-center justify-between px-6 py-4 bg-white dark:bg-gradient-to-r from-slate-800 to-[rgba(69,85,108,0.4)]  shadow">
+        <header className="flex flex-row items-center justify-between px-6 py-4 bg-white dark:bg-gradient-to-r from-slate-800 to-[rgba(69,85,108,0.4)]  shadow">
             <div className="text-2xl font-bold text-blue-600">
                 {/* Logo */}
                 SkyLux Travel
             </div>
-            <nav>
-                <ul className="flex space-x-6 text-gray-700 font-medium">
+            <nav className='flex space-x-[20px] items-center'>
+                <ul className="flex flex-row space-x-6 text-gray-700  font-medium">
                     <li>
                         <NavLink to="/" className={({isActive})=>{return isActive ? "text-black" : "text-gray-600 hover:text-blue-500 transition"}}>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/register" className={({isActive})=>{return isActive ? "text-black" : "text-gray-600 hover:text-blue-600 transition"}}>Register/Login</NavLink>
                     </li>
                     <li>
                         <NavLink to="/about" className={({isActive})=>{return isActive ? "text-black" : "text-gray-600 hover:text-blue-600 transition"}}>About</NavLink>
@@ -24,18 +26,57 @@ function Header(){
                         <NavLink to="/services" className={({isActive})=>{return isActive ? "text-black" : "text-gray-600 hover:text-blue-600 transition"}}>Services</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/profile"className={({isActive})=>{return isActive ? "text-black" : "text-gray-600 hover:text-blue-600 transition"}}>Profile</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/booking"className={({isActive})=>{return isActive ? "text-black" : "text-gray-600 hover:text-blue-600 transition"}}>Booking</NavLink>
-                    </li>
-                    <li>
                         <NavLink to="/contact"className={({isActive})=>{return isActive ? "text-black" : "text-gray-600 hover:text-blue-600 transition"}}>Contact</NavLink>
                     </li>
                 </ul>
+                {
+                    // display this if false:isLoggedIn
+                    !props.checkLoggedIn &&
+                    (
+                        <ul className=" text-gray-700  font-medium">
+                            <li>
+                                <NavLink to="/register" className={({isActive})=>{return isActive ? "text-black" : "text-gray-600 hover:text-blue-600 transition"}}>Register/Login</NavLink>
+                            </li>
+                        </ul>
+
+                    ) 
+                }
+                {
+                    // display this if true:isLoggedIn
+                    props.checkLoggedIn && 
+                    (
+                        <ul className="flex space-x-6 items-center text-gray-700 font-medium">
+                            <li>
+                                <NavLink to="/profile"className={({isActive})=>{return isActive ? "text-black" : "text-gray-600 hover:text-blue-600 transition"}}>Profile</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/booking"className={({isActive})=>{return isActive ? "text-black" : "text-gray-600 hover:text-blue-600 transition"}}>Booking</NavLink>
+                            </li>
+                            <li>
+                                <button
+                                    className="px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700 transition-colors duration-200 shadow"
+                                    onClick={handleLogoutOut}
+                                >
+                                    Logout
+                                </button>
+                            </li>
+                        </ul>
+                    )
+                }
+
             </nav>
         </header>
     );
 };
+const mapInitializeStateToProps = (state)=>{
+    return {
+        checkLoggedIn: state.isLoggedIn
+    }
+}
 
-export default Header;
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        onLoggingOut: ()=>dispatch({type:"ON_LOGGED_OUT"})
+    }
+}
+export default connect(mapInitializeStateToProps,mapDispatchToProps)(Header);
