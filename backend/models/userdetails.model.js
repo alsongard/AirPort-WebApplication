cd289d04d2277d9db64e2a93636daab39ef6f553
alsongard <alsongard@gmail.com>
2025-07-31 01:43:13 +0300

    modified datatypes

--- backend/models/userdetails.model.js
+++ backend/models/userdetails.model.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 
 const UserDetailSchema = new mongoose.Schema(
     {
+        userId: {ref:"User", type:mongoose.Schema.Types.ObjectId},
         firstName: {type:String, required:true, trim:true},
         middleName: {type:String, required:true, trim:true},
         lastName: {type:String, required:true, trim:true},
         age: {type:Number, required:true, trim:true},
         nationality: {type:Object, required:true, trim:true},
         passportNumber: {type:String, required:true, trim:true},
-        userId: {ref:"User", type:mongoose.Schema.Types.ObjectId},
-        phone: {type:Number, required:true},
+        phone: {type:String, required:true},
         dateBirth: {type:Date, required:true},
-        emergencyContact:{type:Number, required:true},
+        emergencyContact:{type:String, required:true},
         emergencyContactName: {type:String, required:true}
     },
     {
