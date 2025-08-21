30f8cf01bf48e2b8520cf62413d588a8bff545e6
alsongard <alsongard@gmail.com>
2025-08-21 08:40:04 +0300

    added getAllBookings router:route

--- backend/routes/booking.router.js
+++ backend/routes/booking.router.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const Booking = require("../models/booking.model");
-const { SetUserBooking, GetUserBooking } = require('../controllers/booking.controller');
+const { SetUserBooking, GetUserBooking, GetAllBookings } = require('../controllers/booking.controller');
 
 
 router.post("/setBooking", SetUserBooking);
 router.get("/getUserBooking/:id", GetUserBooking);
-
+router.get("/getBookings", GetAllBookings)
 
 module.exports = router;
