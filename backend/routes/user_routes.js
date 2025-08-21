fb45c9bbcb33c7de0bb59922a0bad935a0bf5ed5
alsongard <alsongard@gmail.com>
2025-08-21 08:39:09 +0300

    added getAllUsers router:route

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {Register, Login} = require("../controllers/user.controller");
+const {Register, Login, GetAllUsers} = require("../controllers/user.controller");
 const router = express.Router()
 
 
 router.post("/register",Register);
 router.post("/login", Login);
+router.get("/allUser", GetAllUsers);
 
 
 
