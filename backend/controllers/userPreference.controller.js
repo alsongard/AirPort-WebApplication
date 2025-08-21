d7b10f6e773b4263ec54311946df85fb6c1e9fe3
alsongard <alsongard@gmail.com>
2025-08-21 08:38:14 +0300

    userPreference registration function added

--- backend/controllers/userPreference.controller.js
+++ backend/controllers/userPreference.controller.js
@@ -70,5 +70,49 @@ const GetUserPreferences = async (req,res)=>{
     }
 
 }
+const RegisterUserPreferences =  async (req, res)=>{
+    const {userId, seatPreference, mealPreference, classPreference, notifications, newsletter} = req.body;
+    console.log(`userId: ${userId} \n seatPreference: ${seatPreference} \n mealPreference: ${mealPreference} \n classPreference: ${classPreference} \n notifications: ${notifications} \n newsletter: ${newsletter}`);
 
-module.exports = {GetUserPreferences, SetUserPreference}
+    if (!userId || !seatPreference || !mealPreference || !classPreference || !notifications || !newsletter) {
+        return res.status(400).json({success:false, msg:"Invalid INput"});
+    }
+    try
+    {
+        // check if user exists
+        const foundUser = await User.findOne({_id:userId});
+        if (!foundUser)
+        {
+            return res.status(404).json({success:false, msg:`No user found with id: ${userId}`})
+        }
+        // check if userId already has preference
+        const userCheckPreference = await UserPreference.findOne({userId:userId});
+        if (userCheckPreference)
+        {
+            return res.status(409).json({success:false, msg:'User preference already set'})
+        }
+        const new_user_preference = await UserPreference.create({
+            userId: userId,
+            seatPreference: seatPreference,
+            mealPreference: mealPreference,
+            classPreference: classPreference,
+            notifications: notifications,
+            newsletter: newsletter
+        });
+        // console.log(`new_user_preference`); // testing: working
+        console.log('new user preference created');
+        // console.log(new_user_preference);
+        if (!new_user_preference) {
+            return res.status(500).json({success:false, msg:"Server Error"});
+        }
+        return res.status(200).json({success:true, msg:"User preferences saved", data:new_user_preference});
+    }
+    catch(err)
+    {
+        console.log(`Error: ${err}`);
+        return res.status(500).json({success:false, msg:"Internal Server Error"});
+    }
+}
+
+
+module.exports = {GetUserPreferences, RegisterUserPreferences, SetUserPreference}
