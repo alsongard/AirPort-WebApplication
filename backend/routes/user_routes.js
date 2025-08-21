7ce92f304be30b7f9777819c0b8e0f6bff18b7ff
alsongard <alsongard@gmail.com>
2025-08-21 22:03:51 +0300

    added delete user routes:deleteUser

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {Register, Login, GetAllUsers} = require("../controllers/user.controller");
+const {Register, Login, GetAllUsers, DeleteUser} = require("../controllers/user.controller");
 const router = express.Router()
 
 
 router.post("/register",Register);
 router.post("/login", Login);
 router.get("/allUser", GetAllUsers);
+router.delete("/deleteUser/:user_id", DeleteUser);
 
 
 
