445994d60339944d2cdcbf669afa926d24d5a462
alsongard <alsongard@gmail.com>
2025-08-22 15:55:41 +0300

    added role field

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
     {
         email: {type:String, require:true, trim:true},
         password: {type:String, require:true, trim:true},
-
+        role: {type:String, default: "User" , require:true, enum:['User', 'Admin']}
     },
     {
         timestamps: true
