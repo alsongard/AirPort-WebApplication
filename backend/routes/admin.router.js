54707e37c58e1c9a7d600ad8fd58c71d204883a0
alsongard <alsongard@gmail.com>
2025-09-02 05:59:29 +0300

    route for singleFlightdata

--- backend/routes/admin.router.js
+++ backend/routes/admin.router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const {GetAllUsers, DeleteUser,DeleteFlight,  LogAdmin, CreateAdmin} = require("../controllers/admin.controller");
+const {GetAllUsers, DeleteUser,DeleteFlight, SingleFlightData , LogAdmin, CreateAdmin} = require("../controllers/admin.controller");
 
 
 router.get("/users", GetAllUsers )
@@ -8,6 +8,7 @@ router.delete("/deleteUser/:user_id", DeleteUser);
 router.post("/adminLogin", LogAdmin);
 router.post("/createAdmin", CreateAdmin);
 router.delete("/deleteFlight/:id", DeleteFlight);
+route.get("/singleFlight/:id", SingleFlightData)
 
 
 module.exports = router;
